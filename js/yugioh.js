3be1c8360d8faea1f6cd0fa16a5e12bfdc142577
Filliciano Plong <fillicianoplong@gmail.com>
2025-12-19 01:42:44 -0800

    Added displayCards() and init() functions to yugioh.js

--- js/yugioh.js
+++ js/yugioh.js
@@ -12,3 +12,26 @@ export async function getCards() {
         console.error("Error fetching cards: ", error);
     }
 }
+
+function displayCards(cards) {
+    const grid = document.getElementById("card-grid");
+
+    cards.forEach(card => {
+        const cardElement = document.createElement('div');
+        cardElement.className = 'card';
+        cardElement.innerHTML = `
+            <img src="${card.card_images[0].image_url}" alt="${card.name}">
+        `;
+        grid.appendChild(cardElement);
+    });
+}
+
+async function init() {
+    const cards = await getCards(); 
+    
+    if (cards) {
+        displayCards(cards); 
+    }
+}
+
+init();
