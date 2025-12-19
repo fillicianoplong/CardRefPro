c3a411b2d9ef2489b9915d833d49a0d594646ce2
Filliciano Plong <fillicianoplong@gmail.com>
2025-12-19 01:14:04 -0800

    Added getCards() function to yugioh.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const TCG_CONFIG = {
     yugioh: {
-        baseUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php",
+        baseUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?misc=yes",
         itemsPerPage: 50
     }
 };
--- js/yugioh.js
+++ js/yugioh.js
@@ -1,1 +1,9 @@
-import { TCG_CONFIG } from "./config";
+import { TCG_CONFIG } from "./config.js";
+
+async function getCards() {
+    const url = TCG_CONFIG.yugioh.baseUrl;
+    console.log("Fetching from:", url);
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
